1b3eb4f78256da881084848afc632e61c39c1592
Tu Vo <tuvo1106@gmail.com>
2019-07-22 17:08:27 +0000

    Fix spacing in main files

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,12 +7,12 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *bt = NULL;
+	binary_tree_t *bt = NULL;
 
-    bt = calloc(1, sizeof(binary_tree_t));
-    if (!bt)
-        return (NULL);
-    bt->parent = parent;
-    bt->n = value;
-    return (bt);
+	bt = calloc(1, sizeof(binary_tree_t));
+	if (!bt)
+		return (NULL);
+	bt->parent = parent;
+	bt->n = value;
+	return (bt);
 }
