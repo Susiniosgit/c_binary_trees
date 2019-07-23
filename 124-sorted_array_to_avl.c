3885b6786fb3ce09e37eca5b44278ef2a08ca7b6
Tu Vo <tuvo1106@gmail.com>
2019-07-23 19:59:41 +0000

    Add first draft of 124

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,5 +1,26 @@
 #include "binary_trees.h"
 
+
+avl_t *_sorted_array_to_avl(int *array, int start, int end)
+{
+	avl_t *new;
+	int mid;
+
+	if (start > end)
+		return (NULL);
+	mid = (start + end) / 2;
+	new = calloc(1, sizeof(avl_t));
+	if (!new)
+		return (NULL);
+	new->n = array[mid];
+	new->parent = ;
+	printf("new = %i\n", new->n);
+	new->left = _sorted_array_to_avl(array, start, mid - 1);
+	new->right = _sorted_array_to_avl(array, mid + 1, end);
+	return (new);
+}
+
+
 /**
  * sorted_array_to_avl - builds an AVL tree from an array
  * @array: input array
@@ -9,4 +30,7 @@
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 
+	if (!array)
+		return (NULL);
+	return(_sorted_array_to_avl(array, 0, size - 1));
 }
