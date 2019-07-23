14a6ae69569f115dbba58f68d0ae498e6a1b4b87
Tu Vo <tuvo1106@gmail.com>
2019-07-23 03:27:13 +0000

    Betty and refactor bst_remove

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -8,30 +8,78 @@
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-    if (!tree)
-        return (NULL);
-    while (tree)
-    {   
-        if (value == tree->n)
-            return ((bst_t *)tree);
-        tree = value < tree->n ? tree->left
-                               : tree->right;
-    }   
-    return ((bst_t *)tree);
+	if (!tree)
+		return (NULL);
+	while (tree)
+	{
+		if (value == tree->n)
+			return ((bst_t *)tree);
+		tree = value < tree->n ? tree->left
+			: tree->right;
+	}
+	return ((bst_t *)tree);
 }
 
-
+/**
+ * swap - swaps two nodes in binary tree
+ * @node: first node
+ * @new: second node
+ * Return: pointer to root
+ */
 bst_t *swap(bst_t *node, bst_t *new)
 {
 	bst_t *temp = NULL;
 	_Bool left_child = false;
 
-	printf("SWAP: %d with %d\n", node->n, new ? new->n : -1);
 	if (node->parent)
 		left_child = node->parent->left == node;
+	if (new->parent && new->parent != node)
+		new->parent->left = NULL;
+	new->parent = node->parent;
+	if (node->parent)
+	{
+		if (left_child)
+			node->parent->left = new;
+		else
+			node->parent->right = new;
+	}
+	if (node->left != new)
+	{
+		new->left = node->left;
+		node->left->parent = new;
+	}
+	if (node->right && node->right != new)
+	{
+		new->right = node->right;
+		node->right->parent = new;
+	}
+	temp = new;
+	while (temp->parent)
+		temp = temp->parent;
+	free(node);
+	return (temp);
+}
 
-	printf("BEOFRE !NEW\n");
-	if (!new)
+/**
+ * bst_remove - removes a node from a Binary Search Tree
+ * @root: pointer to root of tree
+ * @value: input value
+ * Return: pointer to the new root node of the tree after removing the
+ * desired value
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *node, *temp;
+	_Bool left_child = false;
+
+	if (!root)
+		return (NULL);
+	node = bst_search(root, value);
+	if (!node)
+		return (NULL);
+	if (node->parent)
+		left_child = node->parent->left == node;
+	if (!node->right && !node->left)
 	{
 		if (!node->parent)
 		{
@@ -47,62 +95,12 @@ bst_t *swap(bst_t *node, bst_t *new)
 			temp = temp->parent;
 		free(node);
 		return (temp);
-	}
-	printf("POST !NEW\n");
-		
-	if (new->parent && new->parent != node)
-		new->parent->left = NULL;
-	new->parent = node->parent;
-	if (node->parent)
-	{
-		if (left_child)
-			node->parent->left = new;
-		else
-			node->parent->right = new;
-	}
-	printf("POST IF PARENT\n");
-	
-	if (node->left != new)
-	{
-		new->left = node->left;
-		node->left->parent = new;	
-	}	
-	if (node->right && node->right != new)
-	{
-		new->right = node->right;
-		node->right->parent = new;
-	}
 
-	printf("FOOO\n");
-	temp = new;
-	printf("PRE WHILE\n");
-	while (temp->parent)
-		temp = temp->parent;
-	printf("POST WHILE\n");
-	free(node);
-	return (temp);
-}
-
-/**
- * bst_remove - removes a node from a Binary Search Tree
- * @root: pointer to root of tree
- * @value: input value
- * Return: pointer to the new root node of the tree after removing the
- * desired value
- */
-bst_t *bst_remove(bst_t *root, int value)
-{
-	bst_t *node, *temp;
-
-	if (!root)
-		return (NULL);
-	node = bst_search(root, value);
-	if (!node)
-		return (NULL);
+	}
 	if (!node->right)
-		return swap(node, node->left);
+		return (swap(node, node->left));
 	temp = node->right;
 	while (temp->left)
 		temp = temp->left;
-	return swap(node, temp);
+	return (swap(node, temp));
 }
