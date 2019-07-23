bb5ed103a12392b1f1f32a6148a4d11dfd505a47
Arthur Damm <indigoarthur@gmail.com>
2019-07-23 19:36:09 +0000

    add 111-bst_insert.c

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -8,27 +8,43 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	binary_tree_t *bt = NULL;
+	binary_tree_t *bt = NULL, *node;
 
+	if (!tree)
+		return (NULL);
 	bt = calloc(1, sizeof(binary_tree_t));
 	if (!bt)
 		return (NULL);
 	bt->n = value;
 
-	/* insert as root */
 	if (!*tree)
 	{
 		*tree = bt;
 		return (bt);
 	}
 
-	/* traverse left */
-	if (value < tree->n)
+	node = *tree;
+	while (node)
 	{
-
-	} else
-	{
-
+		if (value == node->n)
+			return (NULL);
+		if (value < node->n)
+		{
+			if (!node->left)
+			{
+				bt->parent = node;
+				return (node->left = bt);
+			}
+			node = node->left;
+		} else
+		{
+			if (!node->right)
+			{
+				bt->parent = node;
+				return (node->right = bt);
+			}
+			node = node->right;
+		}
 	}
-
+	return (NULL);
 }
