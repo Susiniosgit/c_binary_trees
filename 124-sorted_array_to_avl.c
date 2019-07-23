67c7a15edd7d811cc07318ff9e92d97d271b4a70
Tu Vo <tuvo1106@gmail.com>
2019-07-23 20:13:59 +0000

    Update documentation on 124

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,6 +1,13 @@
 #include "binary_trees.h"
 
-
+/**
+ * _sorted_array_to_avl - helper func for sorted_array_to_avl
+ * @array: input array
+ * @start: starting index
+ * @end: ending index
+ * @parent: pointer to parent node
+ * Return: newly created node
+ */
 avl_t *_sorted_array_to_avl(int *array, int start, int end, avl_t *parent)
 {
 	avl_t *new;
@@ -31,5 +38,5 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 
 	if (!array)
 		return (NULL);
-	return(_sorted_array_to_avl(array, 0, size - 1, NULL));
+	return (_sorted_array_to_avl(array, 0, size - 1, NULL));
 }
